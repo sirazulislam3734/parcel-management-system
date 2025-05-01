820e9638eac0a50db525e4383588af01fd62ec70
sirazulislam3734 <sirazulislam3734@gmail.com>
2025-05-01 20:02:10 +0600

    Update a section title

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const SectionTitle = ({ heading, subTitle}) => {
     return (
-        <div className='lg:w-4/12 mx-auto text-center lg:my-10 md:my-5 my-3'>
+        <div className='lg:w-4/12 mx-auto text-center lg:my-12 md:my-6 my-3'>
             <p className='text-yellow-600 my-4'>{subTitle}</p>
             
             <h3 className='lg:text-3xl border-y-4 lg:py-6 py-2  md:py-4'>{heading}</h3>
